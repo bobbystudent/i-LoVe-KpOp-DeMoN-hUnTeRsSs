6d2eda395e114eac4278fd829721cea5c075c307
bobbystudent <bobbymagi4662@gmail.com>
2025-11-10 19:22:00 -0800

    Add files via upload

---
+++ 67 mango mustrd.py
@@ -0,0 +1,22 @@
+"""
+Year: 2067
+Location: Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch
+Story Desc: Tutor was griddying after getting his massive low taper fade, then he went home and played geometry dash and got a record of 1% on Stereo Madness
+and then farted with the power of mach 85 x 10 to the 97th power and blasted the Earth and all celestal bodies within 500 light years to a fine powder that looks like a skibidi
+toilet with a 75 year edging streak. then time reseted and tutor looked like a big poo that was created at 3 in the morning
+and then was thrown at an old woman crossing the street, after that the grandma farted and flew into the air with the power of goku
+to the 75th power times pie then ERM actuauauauauuallllylylly java 64 bit intager hehehehehehhehe im a sucky nerd. 
+"""
+
+if __name__ == "__main__":
+    hunger = {"Tutor": 1, "Gordon Ramsy": 1.5)
+
+
+Choice = input("""
+Day 67:
+Gordon Ramsey enters the scene, getting Chicken Stars and mangos, but behind his back he has a secret mustard gun, what do you do?"
+1. fart at him to try to repell the wild Gordon,
+2. eat the Chicken stars and mangos and swallow the mustard gun,
+3. clap gordon at 4x speed
+4. go onto git hub
+5. oh no skibidi
